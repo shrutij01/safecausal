1f70624c5da8fba95eead3d8f2ec1fadabbe3f46
shrutijoshi <shrutijoshi98@gmail.com>
2024-05-15 13:38:31 -0400

    moments before crash

--- utils.py
+++ utils.py
@@ -1,15 +1,24 @@
-from datasets import ana
+from datasets import load_dataset
+
+from data import ana
 
 
 
-def load_toy_dataset(dataset_name, **kwargs):
+def load_dataset(dataset_name, **kwargs):
     if dataset_name == "ana":
         return ana.generate_data(**kwargs)
+    else:
+        if dataset_name == "truthful_qa":
+            dataset_params = {
+                'split': 'validation',
+                'name': 'multiple_choice',     
+            }
+        dataset = load_dataset(dataset_name, **dataset_params)    
 
 
 def append_instruction(contexts, instruction):
     instruction_plus_contexts = []
     for context in contexts:
-        instruction_plus_contexts.append([instruction, context, 0])
+        instruction_plus_contexts.append([str(instruction) + ' ' + str(context)])
     return instruction_plus_contexts
 
