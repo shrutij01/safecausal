75f9871a3ed82937484eae267781dfc411e8b1da
Shruti Joshi <shrutijoshi98@gmail.com>
2024-07-31 14:48:50 -0400

    eval tt

--- psp/evaluate.py
+++ psp/evaluate.py
@@ -21,10 +21,14 @@ class Evaluator:
                 self.delta_c_test, self.delta_c_hat_test
             )
             print(f"MCC b/w delta_c and delta_c_hat: {mcc_latents}")
+            r2_latents = metrics.r2(self.delta_c_test, self.delta_c_hat_test)
+            print(f"R2 b/w delta_c and delta_c_hat: {r2_latents}")
 
         if self.w_d_gt is not None:
             mcc_encoding = metrics.mean_corr_coef(self.w_d, self.w_d_gt)
             print(f"MCC b/w gt_encoding and learnt_encoding: {mcc_encoding}")
+            r2_encoding = metrics.r2(self.delta_c_test, self.delta_c_hat_test)
+            print(f"R2 b/w gt_encoding and learnt_encoding: {r2_encoding}")
 
     def get_metric_bounds(self):
         enc_gt = np.linalg.inv(self.w_d_gt)
@@ -37,4 +41,7 @@ class Evaluator:
         mcc_gt_enc = metrics.mean_corr_coef(
             self.delta_c_test, delta_c_hat_enc_gt
         )
+        import ipdb
+
+        ipdb.set_trace()
         print(f"MCC b/w delta_c_enc_gt and delta_c: {mcc_gt_enc}")
