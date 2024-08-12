585fee012caf90579fef8a825702bf62f640fb5e
Shruti Joshi <shrutijoshi98@gmail.com>
2024-08-12 19:45:49 -0400

    lang model eval gradeschooler sbatch

--- scripts/sweep.py
+++ scripts/sweep.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         ["--num-epochs 1100"],
     ]
 
-    init_commands = f"source /home/mila/j/joshi.shruti/venvs/eqm/bin/activate && module load miniconda/3 && conda activate pytorch && export PYTHONPATH="/home/mila/j/joshi.shruti/causalrepl_space/psp:$PYTHONPATH" && cd /home/mila/j/joshi.shruti/causalrepl_space/psp/psp"
+    init_commands = f'source /home/mila/j/joshi.shruti/venvs/eqm/bin/activate && module load miniconda/3 && conda activate pytorch && export PYTHONPATH="/home/mila/j/joshi.shruti/causalrepl_space/psp:$PYTHONPATH" && cd /home/mila/j/joshi.shruti/causalrepl_space/psp/psp'
     python_command = "python sparse_dict.py"
     sbatch_command = f"sbatch --gres=gpu:1 --time=1:00:0 --mem=100G --wrap='"
     all_args = list(itertools.product(*hyperparams))
@@ -42,6 +42,12 @@ if __name__ == "__main__":
             + '"'
         )
         print(job_command)
-        result = subprocess.run(job_command, shell=True, executable="/bin/bash", capture_output=True, text=True)
+        result = subprocess.run(
+            job_command,
+            shell=True,
+            executable="/bin/bash",
+            capture_output=True,
+            text=True,
+        )
         print(result.stdout)
         print(result.stderr)
