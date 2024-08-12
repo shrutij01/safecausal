da4788249cc086d375e3c73b46c24aa0a12bb89d
Shruti Joshi <shrutijoshi98@gmail.com>
2024-08-12 19:22:21 -0400

    lang model eval gradeschooler sbatch

--- scripts/sweep.py
+++ scripts/sweep.py
@@ -5,25 +5,29 @@ if __name__ == "__main__":
     account = "joshi.shruti"
     hyperparams = [
         [
-            "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-25_14-37-56_dgp1",
-            "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-30_22-25-10_dgp1dense",
-            "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-25_14-38-03_dgp2",
-            "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-30_22-25-14_dgp2dense",
+            "/network/scratch/j/joshi.shruti/psp/gradeschooler/2024-07-08_17-03-30"
         ],
-        ["--data-type gt_ent"],
+        # [
+        #     "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-25_14-37-56_dgp1",
+        #     "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-30_22-25-10_dgp1dense",
+        #     "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-25_14-38-03_dgp2",
+        #     "/network/scratch/j/joshi.shruti/psp/toy_translator/2024-07-30_22-25-14_dgp2dense",
+        # ],
+        # ["--data-type gt_ent"],
+        ["--data-type emb"],
         [
-            "--model-type linv",
+            # "--model-type linv",
             "--model-type la",
-            "--model-type nla --act_fxn relu",
-            "--model-type nla --act_fxn leakyrelu",
-            "--model-type nla --act_fxn gelu",
+            # "--model-type nla --act_fxn relu",
+            # "--model-type nla --act_fxn leakyrelu",
+            # "--model-type nla --act_fxn gelu",
         ],
-        ["--num-epochs 900"],
+        ["--num-epochs 1100"],
     ]
 
-    init_commands = f'module load miniconda/3 && source ~/venvs/eqm/bin/activate && conda activate pytorch && export PYTHONPATH="/home/mila/j/joshi.shruti/causalrepl_space/psp:$PYTHONPATH" && cd /home/mila/j/joshi.shruti/causalrepl_space/psp/psp'
+    init_commands = f'source ~/venvs/eqm/bin/activate && module load miniconda/3 && conda activate pytorch && export PYTHONPATH="/home/mila/j/joshi.shruti/causalrepl_space/psp:$PYTHONPATH" && cd /home/mila/j/joshi.shruti/causalrepl_space/psp/psp'
     python_command = "python sparse_dict.py"
-    sbatch_command = f"sbatch --gres=gpu:1 --time=0:10:0 --mem=16G"
+    sbatch_command = f"sbatch --gres=gpu:1 --time=1:00:0 --mem=100G"
     all_args = list(itertools.product(*hyperparams))
     print(f"Total jobs = {len(all_args)}")
     for args in all_args:
