1f300f139df0f5bcdae5d18fab1cc69b719ae0ac
Shruti Joshi <shrutijoshi98@gmail.com>
2024-08-12 20:15:01 -0400

    lang model eval gradeschooler sbatch

--- scripts/job.sh
+++ scripts/job.sh
@@ -6,11 +6,10 @@
 #SBATCH --time=1:00:00
 #SBATCH --mem=100Gb
 
-
 source /home/mila/j/joshi.shruti/venvs/eqm/bin/activate
 module load miniconda/3
 conda activate pytorch
 export PYTHONPATH="/home/mila/j/joshi.shruti/causalrepl_space/psp:$PYTHONPATH"
 cd /home/mila/j/joshi.shruti/causalrepl_space/psp/psp
 
-python sparse_dict.py "/network/scratch/j/joshi.shruti/psp/gradeschooler/2024-07-08_17-03-30" --data-type emb --model-type la --num-epochs 1100
+python sparse_dict.py "$@"
