1c5901e871b2e2cc982a2ab927637175cba805f2
Shruti Joshi <shrutijoshi98@gmail.com>
2024-08-22 17:18:22 -0400

    wip: batch linear sae

--- scripts/test_gradeschooler.py
+++ scripts/test_gradeschooler.py
@@ -21,19 +21,29 @@ def prepare_test_data(args, device):
     data_config_file = os.path.join(args.embedding_dir, "config.yaml")
     with open(data_config_file, "r") as file:
         data_config = Box(yaml.safe_load(file))
+    x_test, tf_ids, num_tfs, cfc1_test, cfc2_test = data_utils.load_test_data(
+        args, data_config
+    )
     model_1_config_file = os.path.join(args.model_dir_1, "models_config.yaml")
     model_2_config_file = os.path.join(args.model_dir_2, "models_config.yaml")
+    model_3_config_file = os.path.join(args.model_dir_3, "models_config.yaml")
     with open(model_1_config_file, "r") as file:
         model_1_config = Box(yaml.safe_load(file))
     with open(model_2_config_file, "r") as file:
         model_2_config = Box(yaml.safe_load(file))
+    with open(model_3_config_file, "r") as file:
+        model_3_config = Box(yaml.safe_load(file))
     model_1 = LinearSAE(
-        input_dim=model_1_config.embedding_size,
-        rep_dim=model_1_config.overcomplete_basis_size,
+        input_dim=x_test.shape[1],
+        rep_dim=x_test.shape[1],
     ).to(device)
     model_2 = LinearSAE(
-        input_dim=model_1_config.embedding_size,
-        rep_dim=model_1_config.overcomplete_basis_size,
+        input_dim=x_test.shape[1],
+        rep_dim=x_test.shape[1],
+    ).to(device)
+    model_3 = LinearSAE(
+        input_dim=x_test.shape[1],
+        rep_dim=x_test.shape[1],
     ).to(device)
     model_1_file = os.path.join(
         args.model_dir_1, "sparse_dict_model", "sparse_dict_model.pth"
@@ -41,35 +51,43 @@ def prepare_test_data(args, device):
     model_2_file = os.path.join(
         args.model_dir_2, "sparse_dict_model", "sparse_dict_model.pth"
     )
+    model_3_file = os.path.join(
+        args.model_dir_3, "sparse_dict_model", "sparse_dict_model.pth"
+    )
     model_1_dict = torch.load(model_1_file)
     model_2_dict = torch.load(model_2_file)
+    model_3_dict = torch.load(model_3_file)
     model_1.load_state_dict(model_1_dict)
     model_2.load_state_dict(model_2_dict)
+    model_3_config.load_state_dict(model_3_dict)
     seeds = []
     seeds.append(model_1_config.seed)
     seeds.append(model_2_config.seed)
+    seeds.append(model_3_config.seed)
 
     delta_z_test = None
-    x_test, tf_ids, num_tfs, cfc1_test, cfc2_test = data_utils.load_test_data(
-        args, data_config
-    )
     delta_z_test = x_test
     delta_z_test = tensorify(delta_z_test, device)
     delta_z_hat_test_1, delta_c_hat_test_1 = model_1(delta_z_test)
-    delta_z_hat_test_2, delta_c_hat_test_2 = model_1(delta_z_test)
+    delta_z_hat_test_2, delta_c_hat_test_2 = model_2(delta_z_test)
+    delta_z_hat_test_3, delta_c_hat_test_3 = model_3(delta_z_test)
     delta_c_hat_test_1 = numpify(delta_c_hat_test_1)
     delta_z_hat_test_1 = numpify(delta_z_hat_test_1)
     delta_c_hat_test_2 = numpify(delta_c_hat_test_2)
     delta_z_hat_test_2 = numpify(delta_z_hat_test_2)
+    delta_c_hat_test_3 = numpify(delta_c_hat_test_3)
+    delta_z_hat_test_3 = numpify(delta_z_hat_test_3)
     delta_z_test = [numpify(delta_z_test)]
     wd_1 = numpify(model_1.decoder.weight.data)
     wd_2 = numpify(model_2.decoder.weight.data)
+    wd_3 = numpify(model_3.decoder.weight.data)
     delta_z_hat_test = [delta_z_hat_test_1, delta_z_hat_test_2]
     delta_c_hat_test = [delta_c_hat_test_1, delta_c_hat_test_2]
-    w_d = [wd_1, wd_2]
+    w_d = [wd_1, wd_2, wd_3]
     b_e = [
         numpify(model_1_dict["bias_encoder"]),
         numpify(model_2_dict["bias_encoder"]),
+        numpify(model_3_dict["bias_encoder"]),
     ]
     return (
         delta_z_test,
