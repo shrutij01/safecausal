7f8b4f9e245ed3af63ef47f6d935d839644ec396
Shruti Joshi <shrutijoshi98@gmail.com>
2024-09-26 21:23:33 -0400

    wip:binary_1

--- scripts/job_binary_1.sh
+++ scripts/job_binary_1.sh
@@ -8,13 +8,13 @@ data_configs=(
     "/network/scratch/j/joshi.shruti/psp/binary_1/binary_1_32_config.yaml"
 )
 alphas=(
-    "--alpha 5" "--alpha 10" "--alpha 15"
+    "--alpha 1" "--alpha 3" "--alpha 5" "--alpha 7" "--alpha 11" "--alpha 0.75" "--alpha 0.5"
 )
 epochs=(
-    "--num-epochs 10000"
+    "--num-epochs 20000"
 )
 primal_lrs=(
-    "--primal-lr 0.0001" "--primal-lr 0.001" "--primal-lr 0.0005"
+    "--primal-lr 0.005" "--primal-lr 0.001" "--primal-lr 0.01"
 )
 indicator_thresholds=(
     "--indicator-threshold 0.1"
@@ -51,34 +51,35 @@ for embedding_file in "${embedding_files[@]}"; do
                 for indicator_threshold in "${indicator_thresholds[@]}"; do
                     for norm_type in "${norm_types[@]}"; do
                         for dual_lr in "${dual_lrs[@]}"; do
-                            for seed in "${seeds[@]}"; do
-                                # Define a script name
-                                script_name="generated_jobs/job_${counter}.sh"
+                            for epoch in "${epochs[@]}"; do
+                                for seed in "${seeds[@]}"; do
+                                    # Define a script name
+                                    script_name="generated_jobs/job_${counter}.sh"
 
-                                # Create a batch script for each job
-                                echo "#!/bin/bash" > "${script_name}"
-                                echo "#SBATCH --job-name=${job_name}_${counter}" >> "${script_name}"
-                                echo "#SBATCH --output=${output}" >> "${script_name}"
-                                echo "#SBATCH --error=${error}" >> "${script_name}"
-                                echo "#SBATCH --time=${time_limit}" >> "${script_name}"
-                                echo "#SBATCH --mem=${memory}" >> "${script_name}"
-                                echo "#SBATCH --gres=${gpu_req}" >> "${script_name}"
-                                echo "" >> "${script_name}"
-                                echo "source /home/mila/j/joshi.shruti/venvs/eqm/bin/activate" >> "${script_name}"
-                                echo "module load miniconda/3" >> "${script_name}"
-                                echo "conda activate pytorch" >> "${script_name}"
-                                echo "export PYTHONPATH=\"/home/mila/j/joshi.shruti/causalrepl_space/psp:\$PYTHONPATH\"" >> "${script_name}"
-                                echo "cd /home/mila/j/joshi.shruti/causalrepl_space/psp/psp" >> "${script_name}"
-                                echo "python linear_sae.py --embeddings-file ${embedding_file} --data-config-file ${data_config} ${alpha} ${primal_lr} ${dual_lr} ${norm_type} ${indicator_threshold} ${seed}" >> "${script_name}"
+                                    # Create a batch script for each job
+                                    echo "#!/bin/bash" > "${script_name}"
+                                    echo "#SBATCH --job-name=${job_name}_${counter}" >> "${script_name}"
+                                    echo "#SBATCH --output=${output}" >> "${script_name}"
+                                    echo "#SBATCH --error=${error}" >> "${script_name}"
+                                    echo "#SBATCH --time=${time_limit}" >> "${script_name}"
+                                    echo "#SBATCH --mem=${memory}" >> "${script_name}"
+                                    echo "#SBATCH --gres=${gpu_req}" >> "${script_name}"
+                                    echo "" >> "${script_name}"
+                                    echo "source /home/mila/j/joshi.shruti/venvs/eqm/bin/activate" >> "${script_name}"
+                                    echo "module load miniconda/3" >> "${script_name}"
+                                    echo "conda activate pytorch" >> "${script_name}"
+                                    echo "export PYTHONPATH=\"/home/mila/j/joshi.shruti/causalrepl_space/psp:\$PYTHONPATH\"" >> "${script_name}"
+                                    echo "cd /home/mila/j/joshi.shruti/causalrepl_space/psp/psp" >> "${script_name}"
+                                    echo "python linear_sae.py --embeddings-file ${embedding_file} --data-config-file ${data_config} ${alpha} ${primal_lr} ${dual_lr} ${norm_type} ${indicator_threshold} ${epoch} ${seed}" >> "${script_name}"
 
-                                # Make the script executable
-                                chmod +x "${script_name}"
+                                    # Make the script executable
+                                    chmod +x "${script_name}"
 
-                                # Submit the job
-                                sbatch "${script_name}"
+                                    # Submit the job
+                                    sbatch "${script_name}"
 
-                                # Increment counter
-                                ((counter++))
+                                    # Increment counter
+                                    ((counter++))
                             done
                         done
                     done
