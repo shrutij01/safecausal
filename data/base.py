503beae347ba64c67a59ad65d7a9f0ccad69afc4
Shruti Joshi <shrutijoshi98@gmail.com>
2024-09-27 01:51:32 -0400

    wip:binary_1

--- data/base.py
+++ data/base.py
@@ -1,4 +1,4 @@
-eng_french_common_objects = [
+binary_1 = [
     ("Friday", "Vendredi"),
     ("Chair", "Chaise"),
     ("Book", "Livre"),
@@ -100,6 +100,568 @@ eng_french_common_objects = [
     ("Vacuum", "Aspirateur"),
     ("Fan", "Ventilateur"),
 ]
+
+binary_1_neg = [
+    ("Bonjour", "Hello"),
+    ("Au revoir", "Goodbye"),
+    ("Merci", "Thank you"),
+    ("S'il vous plaît", "Please"),
+    ("Excusez-moi", "Excuse me"),
+    ("Oui", "Yes"),
+    ("sourcil", "eyebrow"),
+    ("anglais", "English"),
+    ("Serpillière", "Mop"),
+    ("belle", "beautiful"),
+    ("Métro", "Subway"),
+    ("Billet", "Ticket"),
+    ("Combien", "How much"),
+    ("Où", "Where"),
+    ("Quand", "When"),
+    ("Comment", "How"),
+    ("Pourquoi", "Why"),
+    ("Aéroport", "Airport"),
+    ("Gare", "Train station"),
+    ("Baignoire", "Bathtub"),
+    ("Chambre", "Room"),
+    ("Clé", "Key"),
+    ("Carte", "Map"),
+    ("comment", "how"),
+    ("Rue", "Street"),
+    ("paquet", "bundle"),
+    ("Place", "Square"),
+    ("Musée", "Museum"),
+    ("Entrée", "Entrance"),
+    ("Sortie", "Exit"),
+    ("Ouvert", "Open"),
+    ("Fermé", "Closed"),
+    ("Aujourd'hui", "Today"),
+    ("Demain", "Tomorrow"),
+    ("Hier", "Yesterday"),
+    ("Matin", "Morning"),
+    ("Soir", "Evening"),
+    ("Nuit", "Night"),
+    ("myrtille", "blueberry"),
+    ("Vendredi", "Friday"),
+    ("cuisiner", "cook"),
+    ("Quai", "Platform"),
+    ("jachère", "fallow"),
+    ("Gauche", "Left"),
+    ("Droite", "Right"),
+    ("Tout droit", "Straight ahead"),
+    ("Ici", "Here"),
+    ("Là-bas", "Over there"),
+    ("Je voudrais", "I would like"),
+    ("noir", "black"),
+    ("L'addition", "The bill"),
+    ("Eau", "Water"),
+    ("manteau", "coat"),
+    ("Pain", "Bread"),
+    ("Fromage", "Cheese"),
+    ("Café", "Coffee"),
+    ("Thé", "Tea"),
+    ("Sucre", "Sugar"),
+    ("Sel", "Salt"),
+    ("Poivre", "Pepper"),
+    ("Poulet", "Chicken"),
+    ("Bœuf", "Beef"),
+    ("Poisson", "Fish"),
+    ("Légumes", "Vegetables"),
+    ("dormir", "sleep"),
+    ("hiver", "winter"),
+    ("Glace", "Ice cream"),
+    ("Gâteau", "Cake"),
+    ("merde", "shit"),
+    ("Boulangerie", "Bakery"),
+    ("ete", "summer"),
+    ("nager", "swim"),
+    ("cheveux", "hair"),
+    ("Urgence", "Emergency"),
+    ("Aide", "Help"),
+    ("Argent", "Money"),
+    ("paresseuse", "lazy"),
+    ("maladie", "disease"),
+    ("mer", "sea"),
+    ("Cher", "Expensive"),
+    ("Bon marché", "Cheap"),
+    ("Numéro", "Number"),
+    ("chic", "classy"),
+    ("lunettes", "spectacles"),
+    ("meurtre", "murder"),
+    ("herbe", "weed"),
+    ("drôle", "funny"),
+    ("Douane", "Customs"),
+    ("Bagage", "Luggage"),
+    ("Valise", "Suitcase"),
+    ("Perdu", "Lost"),
+    ("Trouvé", "Found"),
+    ("confiance", "trust"),
+    ("garçon", "boy"),
+    ("Heure", "Time"),
+    ("sable", "sand"),
+    ("intégrer", "embed"),
+    ("Soleil", "Sun"),
+    ("Pluie", "Rain"),
+    ("poumons", "lungs"),
+    ("foie", "liver"),
+    ("Amis", "Friends"),
+    ("mère", "mother"),
+    ("Cœur", "Heart"),
+    ("jaune", "yellow"),
+    ("J'aime", "I like"),
+    ("verte", "green"),
+    ("Pont", "Bridge"),
+    ("rouge", "red"),
+    ("Château", "Castle"),
+    ("fraise", "strawberry"),
+]
+
+binary_2 = [
+    [
+        "King",
+        "Reine",
+    ],  # English masculine word, French feminine translation (Queen)
+    ["Père", "Mother"],  # French masculine word, English feminine translation
+    [
+        "Brother",
+        "Sœur",
+    ],  # English masculine word, French feminine translation (Sister)
+    ["Tante", "Uncle"],  # French feminine word, English masculine translation
+    [
+        "Prince",
+        "Princesse",
+    ],  # English masculine word, French feminine translation (Princess)
+    [
+        "Actrice",
+        "Actor",
+    ],  # French feminine word, English masculine translation
+    [
+        "Serveuse",
+        "Waiter",
+    ],  # French feminine word, English masculine translation
+    ["Déesse", "God"],  # French feminine word, English masculine translation
+    [
+        "Wizard",
+        "Sorcière",
+    ],  # English masculine word, French feminine translation (Witch)
+    [
+        "Impératrice",
+        "Emperor",
+    ],  # French feminine word, English masculine translation
+    [
+        "Monk",
+        "Nonne",
+    ],  # English masculine word, French feminine translation (Nun)
+    ["Hôtesse", "Host"],  # French feminine word, English masculine translation
+    [
+        "Boy",
+        "Fille",
+    ],  # English masculine word, French feminine translation (Girl)
+    [
+        "Duchesse",
+        "Duke",
+    ],  # French feminine word, English masculine translation
+    [
+        "Widower",
+        "Veuve",
+    ],  # English masculine word, French feminine translation (Widow)
+    [
+        "Jument",
+        "Stallion",
+    ],  # French feminine word, English masculine translation
+    ["Vache", "Bull"],  # French feminine word, English masculine translation
+    [
+        "Rooster",
+        "Poule",
+    ],  # English masculine word, French feminine translation (Hen)
+    [
+        "Mariée",
+        "Bridegroom",
+    ],  # French feminine word, English masculine translation
+    [
+        "Dame",
+        "Gentleman",
+    ],  # French feminine word, English masculine translation
+    [
+        "Grandfather",
+        "Grand-mère",
+    ],  # English masculine word, French feminine translation (Grandmother)
+    ["Madame", "Sir"],  # French feminine word, English masculine translation
+    [
+        "Baron",
+        "Baronne",
+    ],  # English masculine word, French feminine translation (Baroness)
+    [
+        "Bergère",
+        "Shepherd",
+    ],  # French feminine word, English masculine translation
+    [
+        "Prêtresse",
+        "Priest",
+    ],  # French feminine word, English masculine translation
+    ["Lionne", "Lion"],  # French feminine word, English masculine translation
+    [
+        "Tiger",
+        "Tigresse",
+    ],  # English masculine word, French feminine translation (Tigress)
+    [
+        "Waiter",
+        "Infirmière",
+    ],  # English masculine word, French feminine translation (Nurse)
+    [
+        "Paonne",
+        "Peacock",
+    ],  # French feminine word, English masculine translation
+    ["Renarde", "Fox"],  # French feminine word, English masculine translation
+    [
+        "Marraine",
+        "Godfather",
+    ],  # French feminine word, English masculine translation
+    [
+        "Actor",
+        "Poétesse",
+    ],  # English masculine word, French feminine translation (Poetess)
+    [
+        "Duchesse",
+        "Wizard",
+    ],  # French feminine word, English masculine translation
+    [
+        "Queen",
+        "Roi",
+    ],  # English feminine word, French masculine translation (King)
+    ["Mère", "Father"],  # French feminine word, English masculine translation
+    [
+        "Sister",
+        "Frère",
+    ],  # English feminine word, French masculine translation (Brother)
+    ["Oncle", "Aunt"],  # French masculine word, English feminine translation
+    [
+        "Princess",
+        "Prince",
+    ],  # English feminine word, French masculine translation
+    [
+        "Acteur",
+        "Actress",
+    ],  # French masculine word, English feminine translation
+    [
+        "Waitress",
+        "Serveur",
+    ],  # English feminine word, French masculine translation (Waiter)
+    ["Neveu", "Niece"],  # French masculine word, English feminine translation
+    [
+        "Heroine",
+        "Héros",
+    ],  # English feminine word, French masculine translation (Hero)
+    [
+        "Goddess",
+        "Dieu",
+    ],  # English feminine word, French masculine translation (God)
+    [
+        "Witch",
+        "Sorcier",
+    ],  # English feminine word, French masculine translation (Wizard)
+    [
+        "Empereur",
+        "Empress",
+    ],  # French masculine word, English feminine translation
+    [
+        "Nun",
+        "Moine",
+    ],  # English feminine word, French masculine translation (Monk)
+    ["Hôte", "Hostess"],  # French masculine word, English feminine translation
+    [
+        "Girl",
+        "Garçon",
+    ],  # English feminine word, French masculine translation (Boy)
+    ["Duc", "Duchess"],  # French masculine word, English feminine translation
+    [
+        "Widow",
+        "Veuf",
+    ],  # English feminine word, French masculine translation (Widower)
+    [
+        "Mare",
+        "Étalon",
+    ],  # English feminine word, French masculine translation (Stallion)
+    ["Taureau", "Cow"],  # French masculine word, English feminine translation
+    [
+        "Hen",
+        "Coq",
+    ],  # English feminine word, French masculine translation (Rooster)
+    [
+        "Bride",
+        "Marié",
+    ],  # English feminine word, French masculine translation (Groom)
+    [
+        "Gentilhomme",
+        "Lady",
+    ],  # French masculine word, English feminine translation
+    [
+        "Grandmother",
+        "Grand-père",
+    ],  # English feminine word, French masculine translation (Grandfather)
+    [
+        "Monsieur",
+        "Madam",
+    ],  # French masculine word, English feminine translation
+    [
+        "Baron",
+        "Baroness",
+    ],  # French masculine word, English feminine translation
+    [
+        "Shepherdess",
+        "Berger",
+    ],  # English feminine word, French masculine translation (Shepherd)
+    [
+        "Prêtre",
+        "Priestess",
+    ],  # French masculine word, English feminine translation
+    ["Lioness", "Lion"],  # English feminine word, French masculine translation
+    [
+        "Tigress",
+        "Tigre",
+    ],  # English feminine word, French masculine translation (Tiger)
+    [
+        "Policier",
+        "Policewoman",
+    ],  # French masculine word, English feminine translation
+    [
+        "Peahen",
+        "Paon",
+    ],  # English feminine word, French masculine translation (Peacock)
+    ["Renard", "Vixen"],  # French masculine word, English feminine translation
+    [
+        "Parrain",
+        "Godmother",
+    ],  # French masculine word, English feminine translation
+    [
+        "Poète",
+        "Actress",
+    ],  # French masculine word, English feminine translation
+    [
+        "Duchess",
+        "Wizard",
+    ],  # English feminine word, French masculine translation
+    ["Emperor", "Impératrice"],
+    ["Fils", "Daughter"],
+    ["Mari", "Wife"],
+    ["Marié", "Bride"],
+    ["Dog", "Chienne"],
+    ["Stag", "Biche"],
+    ["Canard", "Duck"],
+    ["Garçon", "Girl"],
+    ["Homme", "Woman"],
+    # only_gender_english = [
+    ["actor", "actress"],
+    ["batman", "batwoman"],
+    ["boar", "sow"],
+    ["boy", "girl"],
+    ["brother", "sister"],
+    ["buck", "doe"],
+    ["bull", "cow"],
+    ["businessman", "businesswoman"],
+    ["chairman", "chairwoman"],
+    ["dad", "mom"],
+    ["daddy", "mommy"],
+    ["duke", "duchess"],
+    ["emperor", "empress"],
+    ["father", "mother"],
+    ["fisherman", "fisherwoman"],
+    ["fox", "vixen"],
+    ["gentleman", "lady"],
+    ["god", "goddess"],
+    ["grandfather", "grandmother"],
+    ["grandpa", "grandma"],
+    ["grandson", "granddaughter"],
+    ["groom", "bride"],
+    ["he", "she"],
+    ["headmaster", "headmistress"],
+    ["heir", "heiress"],
+    ["hero", "heroine"],
+    ["husband", "wife"],
+    ["king", "queen"],
+    ["lion", "lioness"],
+    ["man", "woman"],
+    ["manager", "manageress"],
+    ["men", "women"],
+    ["mister", "miss"],
+    ["murderer", "murderess"],
+    ["nephew", "niece"],
+    ["poet", "poetess"],
+    ["policeman", "policewoman"],
+    ["prince", "princess"],
+    ["ram", "ewe"],
+    ["rooster", "hen"],
+    ["sculptor", "sculptress"],
+    ["sir", "madam"],
+    ["son", "daughter"],
+    ["stallion", "mare"],
+    ["stepfather", "stepmother"],
+    ["superman", "superwoman"],
+    ["tiger", "tigress"],
+    ["uncle", "aunt"],
+    ["valet", "maid"],
+    ["waiter", "waitress"],
+    ["webmaster", "webmistress"],
+    # Additional unique pairs
+    ["wizard", "witch"],
+    ["landlord", "landlady"],
+    ["host", "hostess"],
+    ["priest", "priestess"],
+    ["abbot", "abbess"],
+    ["count", "countess"],
+    ["monk", "nun"],
+    ["lad", "lass"],
+    ["male", "female"],
+    ["peacock", "peahen"],
+    ["steward", "stewardess"],
+    ["master", "mistress"],
+    ["widower", "widow"],
+    ["sultan", "sultana"],
+    ["earl", "countess"],
+    ["son-in-law", "daughter-in-law"],
+    ["fiancé", "fiancée"],
+    ["lord", "lady"],
+    ["bachelor", "spinster"],
+    ["milkman", "milkmaid"],
+    ["manservant", "maidservant"],
+    ["baron", "baroness"],
+    ["salesman", "saleswoman"],
+    ["conductor", "conductress"],
+    ["godfather", "godmother"],
+    ["boyfriend", "girlfriend"],
+    ["shepherd", "shepherdess"],
+    ["papa", "mama"],
+    ["stepson", "stepdaughter"],
+    ["godson", "goddaughter"],
+    ["testator", "testatrix"],
+    ["executor", "executrix"],
+    ["hunter", "huntress"],
+    ["mayor", "mayoress"],
+    ["peer", "peeress"],
+    ["songster", "songstress"],
+    ["tempter", "temptress"],
+    ["adventurer", "adventuress"],
+    ["proprietor", "proprietress"],
+    ["giant", "giantess"],
+    ["marquis", "marchioness"],
+    ["czar", "czarina"],
+    ["author", "authoress"],
+    ["enchanter", "enchantress"],
+    ["seamster", "seamstress"],
+    ["prophet", "prophetess"],
+    # only_gender_french = [
+    ["roi", "reine"],  # king, queen
+    ["père", "mère"],  # father, mother
+    ["frère", "sœur"],  # brother, sister
+    ["oncle", "tante"],  # uncle, aunt
+    ["fils", "fille"],  # son, daughter
+    ["homme", "femme"],  # man, woman
+    ["garçon", "fille"],  # boy, girl
+    ["mari", "femme"],  # husband, wife
+    ["acteur", "actrice"],  # actor, actress
+    ["serveur", "serveuse"],  # waiter, waitress
+    ["hôte", "hôtesse"],  # host, hostess
+    ["dieu", "déesse"],  # god, goddess
+    ["héros", "héroïne"],  # hero, heroine
+    ["empereur", "impératrice"],  # emperor, empress
+    ["lion", "lionne"],  # lion, lioness
+    ["taureau", "vache"],  # bull, cow
+    ["cheval", "jument"],  # horse, mare
+    ["sorcier", "sorcière"],  # wizard, witch
+    ["moine", "nonne"],  # monk, nun
+    ["monsieur", "madame"],  # mister, madam
+    ["baron", "baronne"],  # baron, baroness
+    ["neveu", "nièce"],  # nephew, niece
+    ["grand-père", "grand-mère"],  # grandfather, grandmother
+    ["veuf", "veuve"],  # widower, widow
+    ["fiancé", "fiancée"],  # fiancé, fiancée
+    ["prince", "princesse"],  # prince, princess
+    ["duc", "duchesse"],  # duke, duchess
+    ["ami", "amie"],  # friend
+    ["voisin", "voisine"],  # neighbor
+    ["cousin", "cousine"],  # cousin
+    ["étudiant", "étudiante"],  # student
+    ["jumeau", "jumelle"],  # twin
+    ["boulanger", "boulangère"],  # baker
+    ["chanteur", "chanteuse"],  # singer
+    ["danseur", "danseuse"],  # dancer
+    ["vendeur", "vendeuse"],  # seller
+    ["docteur", "doctoresse"],  # doctor
+    ["ambassadeur", "ambassadrice"],  # ambassador
+    ["paysan", "paysanne"],  # peasant
+    ["tigre", "tigresse"],  # tiger, tigress
+    ["chien", "chienne"],  # dog
+    ["chat", "chatte"],  # cat
+    ["coq", "poule"],  # rooster, hen
+    ["bouc", "chèvre"],  # billy goat, goat
+    ["maître", "maîtresse"],  # master, mistress
+    ["lecteur", "lectrice"],  # reader
+    ["menteur", "menteuse"],  # liar
+    ["directeur", "directrice"],  # director
+    ["créateur", "créatrice"],  # creator
+    ["conducteur", "conductrice"],  # driver
+    ["cuisinier", "cuisinière"],  # cook
+    ["patron", "patronne"],  # boss
+    ["jardinier", "jardinière"],  # gardener
+    ["serveur", "serveuse"],  # waiter, waitress
+    ["boucher", "bouchère"],  # butcher
+    ["policier", "policière"],  # police officer
+    ["magicien", "magicienne"],  # magician
+    ["avocat", "avocate"],  # lawyer
+    ["professeur", "professeure"],  # teacher
+    ["ingénieur", "ingénieure"],  # engineer
+    ["docteur", "docteure"],  # doctor
+    ["président", "présidente"],  # president
+    ["artisan", "artisane"],  # artisan
+    ["comédien", "comédienne"],  # comedian
+    ["époux", "épouse"],  # husband, wife
+    ["général", "générale"],  # general
+    ["orateur", "oratrice"],  # speaker
+    ["pompier", "pompière"],  # firefighter
+    ["poète", "poétesse"],  # poet
+    ["chanteur", "chanteuse"],  # singer
+    ["agriculteur", "agricultrice"],  # farmer
+    ["vendeur", "vendeuse"],  # seller
+    ["musicien", "musicienne"],  # musician
+    ["boucher", "bouchère"],  # butcher
+    ["opticien", "opticienne"],  # optician
+    [
+        "sage-femme",
+        "maïeuticien",
+    ],  # midwife (note: male midwife is rare and called "maïeuticien")
+    ["hôte", "hôtesse"],  # host, hostess
+    ["aviateur", "aviatrice"],  # aviator
+    ["inspecteur", "inspectrice"],  # inspector
+    ["écrivain", "écrivaine"],  # writer
+    ["compositeur", "compositrice"],  # composer
+    ["éditeur", "éditrice"],  # editor
+    ["fabricant", "fabricante"],  # manufacturer
+    ["gérant", "gérante"],  # manager
+    ["chasseur", "chasseuse"],  # hunter
+    ["administrateur", "administratrice"],  # administrator
+    ["ambassadeur", "ambassadrice"],  # ambassador
+    ["bâtisseur", "bâtisseuse"],  # builder
+    ["commandant", "commandante"],  # commander
+    ["conseiller", "conseillère"],  # advisor
+    ["défenseur", "défenseure"],  # defender
+    ["entraîneur", "entraîneuse"],  # coach
+    ["gouverneur", "gouverneure"],  # governor
+    ["investisseur", "investisseuse"],  # investor
+    ["maçon", "maçonne"],  # mason
+    ["négociateur", "négociatrice"],  # negotiator
+    ["pêcheur", "pêcheuse"],  # fisherman
+    ["programmateur", "programmatrice"],  # programmer
+    ["représentant", "représentante"],  # representative
+    ["sage", "sage"],  # wise person (same word)
+    ["surveillant", "surveillante"],  # supervisor
+    ["traducteur", "traductrice"],  # translator
+    ["abbé", "abbesse"],
+    ["collègue", "consœur"],
+    ["cochon", "truie"],
+    ["loup", "louve"],
+    ["fondateur", "fondatrice"],
+    ["développeur", "développeuse"],
+]
+
 
 CATEGORICALCODEBOOK = {
     "object": [
