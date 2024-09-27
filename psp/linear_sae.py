2e7919068302da8fb0ac24c01eded28706eb506a
Shruti Joshi <shrutijoshi98@gmail.com>
2024-09-27 00:09:27 -0400

    wip:binary_1

--- psp/linear_sae.py
+++ psp/linear_sae.py
@@ -99,8 +99,7 @@ class LinearSAE(nn.Module):
         delta_z, info = self.preprocess(delta_z)
 
         concept_indicators = (
-            self.encoder_ln(self.act(self.encoder(delta_z)))
-            + self.encoder_bias
+            self.encoder_ln((self.encoder(delta_z))) + self.encoder_bias
         )
         delta_z_hat = self.decoder(concept_indicators) + self.decoder_bias
         delta_z_hat = delta_z_hat * info["std"] + info["mu"]
