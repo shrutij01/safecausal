5d430265bb38314e8785cf9b03b736ff03837ab8
Shruti Joshi <shrutijoshi98@gmail.com>
2024-09-27 02:03:39 -0400

    wip:binary_1

--- scripts/job_binary_1.sh
+++ scripts/job_binary_1.sh
@@ -2,13 +2,13 @@
 
 # Define hyperparameters
 embedding_files=(
-    "/network/scratch/j/joshi.shruti/psp/binary_1/binary_1_embeddings_layer_13.h5"
+    "/network/scratch/j/joshi.shruti/psp/binary_1/binary_1_embeddings_layer_32.h5"
 )
 data_configs=(
-    "/network/scratch/j/joshi.shruti/psp/binary_1/binary_1_13_config.yaml"
+    "/network/scratch/j/joshi.shruti/psp/binary_1/binary_1_32_config.yaml"
 )
 alphas=(
-    "--alpha 11" "--alpha 15" "--alpha 21"
+    "--alpha 15" "--alpha 21"
 )
 epochs=(
     "--num-epochs 20000"
