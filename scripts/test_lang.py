27a4cd62032852952547f19bb3bd70b5b9089cc8
Shruti Joshi <shrutijoshi98@gmail.com>
2024-09-29 00:38:38 -0400

    trubaseline

--- scripts/test_lang.py
+++ scripts/test_lang.py
@@ -38,8 +38,13 @@ def load_baseline(modeldir, dataconfig):
 
 
 def load_model(modeldir, dataconfig):
+    # with open(
+    #     os.path.join(modeldir, "prebias/", "model_config.yaml"),
+    #     "r",
+    # ) as file:
+    #     modelconfig = Box(yaml.safe_load(file))
     with open(
-        os.path.join(modeldir, "prebias/", "model_config.yaml"),
+        os.path.join(modeldir, "model_config.yaml"),
         "r",
     ) as file:
         modelconfig = Box(yaml.safe_load(file))
@@ -48,16 +53,19 @@ def load_model(modeldir, dataconfig):
         num_concepts=dataconfig.num_concepts,
         norm_type="bn",
     ).to(device)
+    # model.load_state_dict(
+    #     # torch.load(os.path.join(modeldir, "prebias/", "sparse_dict_model.pth"))
+    # )
     model.load_state_dict(
-        torch.load(os.path.join(modeldir, "prebias/", "sparse_dict_model.pth"))
+        torch.load(os.path.join(modeldir, "sparse_dict_model.pth"))
     )
     model.eval()
-    model_string = str(modelconfig.alpha) + "_" + str(modelconfig.primal_lr)
+    # model_string = str(modelconfig.alpha) + "_" + str(modelconfig.primal_lr)
     return (
         model,
         utils.numpify(model.decoder.weight.data),
         int(modelconfig.seed),
-        model_string,
+        # model_string,
     )
 
 
@@ -95,7 +103,8 @@ def main(args):
     ]
     models, wds, seeds = [], [], []
     for modeldir in modeldirs:
-        model, wd, seed, model_string = load_model(modeldir, data_config)
+        # model, wd, seed, model_string = load_model(modeldir, data_config)
+        model, wd, seed = load_model(modeldir, data_config)
         models.append(model)
         wds.append(wd)
         seeds.append(seed)
@@ -110,6 +119,7 @@ def main(args):
         mccs = compute_mccs(seeds, wds)
         mean_mcc = np.mean(mccs, axis=0)
         std_mcc = np.std(mccs, axis=0)
+        model_string = "ok"
         mcc_string = "mccs_" + str(model_string)
         mean_mcc_string = "mean_mcc_" + str(model_string)
         std_mcc_string = "std_mcc_" + str(model_string)
@@ -202,7 +212,90 @@ def main(args):
         import ipdb
 
         ipdb.set_trace()
-    if data_config.dataset == "binary_2":
+        data = np.vstack([tilde_z, z_md, z_neta])
+
+        # Create labels for each set
+        labels = np.array(
+            ["tilde_z"] * tilde_z.shape[0]
+            + ["z_md"] * z_md.shape[0]
+            + ["z_neta"] * z_neta.shape[0]
+        )
+
+        # Step 2: Apply t-SNE
+        tsne = TSNE(n_components=2, perplexity=2, random_state=42)
+        transformed_data = tsne.fit_transform(data)
+
+        # Step 3: Plot the results
+        plt.figure(figsize=(10, 8))
+        sns.scatterplot(
+            x=transformed_data[:, 0],
+            y=transformed_data[:, 1],
+            hue=labels,
+            style=labels,
+            palette="viridis",
+            s=100,
+        )
+        plt.title("t-SNE visualization of tilde_z, z_md, z_neta")
+        plt.xlabel("t-SNE 1")
+        plt.ylabel("t-SNE 2")
+        plt.legend(title="Steering")
+        plt.grid(True)
+        plt.savefig("tsne_" + str(data_config.dataset) + "_" + ".png")
+    elif data_config.dataset == "binary_2":
+        compute_mccs(seeds, wds)
+        neta = neta / np.linalg.norm(neta)
+        z = z / np.linalg.norm(z)
+        z_neta = z + neta
+        z_neta = z_neta / np.linalg.norm(z_neta)
+        tilde_z = tilde_z / np.linalg.norm(tilde_z)
+        cosines_neta = []
+        for i in range(tilde_z.shape[0]):
+            cosines_neta.append(
+                cosine_similarity(
+                    tilde_z[i].reshape(1, -1), z_neta[i].reshape(1, -1)
+                )
+            )
+        cosines_neta = [float(arr[0][0]) for arr in cosines_neta]
+        plt.figure(figsize=(10, 6))
+        sns.kdeplot(
+            cosines_neta,
+            bw_adjust=0.75,
+            label="Cosine Similarity with neta",
+            shade=True,
+        )
+        plt.title("KDE of Cosine Similarities")
+        plt.xlabel("Cosine Similarity")
+        plt.ylabel("Density")
+        plt.legend()
+        plt.savefig("kde_" + str(data_config.dataset) + "_" + ".png")
+        data = np.vstack([tilde_z, z_neta])
+
+        # Create labels for each set
+        labels = np.array(
+            ["tilde_z"] * tilde_z.shape[0] + ["z_neta"] * z_neta.shape[0]
+        )
+
+        # Step 2: Apply t-SNE
+        tsne = TSNE(n_components=2, perplexity=5, random_state=42)
+        transformed_data = tsne.fit_transform(data)
+
+        # Step 3: Plot the results
+        plt.figure(figsize=(10, 8))
+        sns.scatterplot(
+            x=transformed_data[:, 0],
+            y=transformed_data[:, 1],
+            hue=labels,
+            style=labels,
+            palette="viridis",
+            s=100,
+        )
+        plt.title("t-SNE visualization of tilde_z, z_neta")
+        plt.xlabel("t-SNE 1")
+        plt.ylabel("t-SNE 2")
+        plt.legend(title="Steering")
+        plt.grid(True)
+        plt.savefig("tsne_" + str(data_config.dataset) + "_" + ".png")
+        # test steering vectors on 1-sparse data
         onesp_tilde_z, onesp_z = utils.load_test_data(
             data_file=args.data_file2,
         )
@@ -265,9 +358,9 @@ if __name__ == "__main__":
     parser.add_argument("modeldir_1")
     parser.add_argument("modeldir_2")
     parser.add_argument("modeldir_3")
-    parser.add_argument("modeldir_4")
-    parser.add_argument("modeldir_5")
-    parser.add_argument("baseline")
+    # parser.add_argument("modeldir_4")
+    # parser.add_argument("modeldir_5")
+    # parser.add_argument("baseline")
     parser.add_argument(
         "--data-file2",
         default="/network/scratch/j/joshi.shruti/psp/binary_1/binary_1_32_config.yaml",
