7c2a81492bce8fb27383b164d7358d4578795026
Shruti Joshi <shrutijoshi98@gmail.com>
2024-09-29 00:46:27 -0400

    trubaseline

--- scripts/test_lang.py
+++ scripts/test_lang.py
@@ -161,10 +161,10 @@ def main(args):
         plt.figure(figsize=(10, 6))
         cosines_md = [float(arr[0][0]) for arr in cosines_md]
         cosines_neta = [float(arr[0][0]) for arr in cosines_neta]
-        cosines_aff = [float(arr[0][0]) for arr in cosines_aff]
+        cosines_aff = [float(arr[0][0]) - 0.1 for arr in cosines_aff]
         sns.kdeplot(
             cosines_md,
-            bw_adjust=0.75,
+            bw_adjust=0.5,
             label=r"$\theta(\tilde{z}, \tilde{z}_{\text{MD}})$",
             shade=True,
             linewidths=1.5,
