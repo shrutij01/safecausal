c4516a8ef995469539b94c0ae0d7920373d44db7
Shruti Joshi <shrutijoshi98@gmail.com>
2024-09-30 10:52:29 -0400

    mcc for baselines

--- scripts/test_lang.py
+++ scripts/test_lang.py
@@ -88,8 +88,8 @@ def main(args):
         args.modeldir_1,
         args.modeldir_2,
         args.modeldir_3,
-        args.modeldir_4,
-        args.modeldir_5,
+        # args.modeldir_4,
+        # args.modeldir_5,
     ]
 
     modeldirs = [
@@ -313,8 +313,8 @@ if __name__ == "__main__":
     parser.add_argument("modeldir_1")
     parser.add_argument("modeldir_2")
     parser.add_argument("modeldir_3")
-    parser.add_argument("modeldir_4")
-    parser.add_argument("modeldir_5")
+    # parser.add_argument("modeldir_4")
+    # parser.add_argument("modeldir_5")
     parser.add_argument("baseline")
     parser.add_argument(
         "--data-file2",
