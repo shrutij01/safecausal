8839d6dc2011c29157f8d17f5e2ae1eee513ff96
Shruti Joshi <shrutijoshi98@gmail.com>
2024-11-26 15:19:54 -0500

    keeping track of the plotting script

--- cosplot.py
+++ cosplot.py
@@ -1,16 +1,16 @@
 import numpy as np
 import argparse
 
-import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-import matplotlib.ticker as ticker
 
-
-matplotlib.rcParams["text.usetex"] = True
-
-matplotlib.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"
+plt.rcParams.update(
+    {
+        "text.usetex": True,
+        "text.latex.preamble": r"\usepackage{amsmath}",
+    }
+)
 plt.rc("font", family="serif", size=19)
 
 
@@ -38,6 +38,7 @@ def get_data(type="same"):
             r"$\textbf{\textsc{CAT}(135, 3)}$",
             r"$\textbf{\text{TruthfulQA}(1, 1)}$",
         ]
+        dataset_positions = np.array([2 * i + 1 for i in range(len(labels))])
     elif type == "ood":
         values_eta = np.array(
             [
@@ -53,9 +54,10 @@ def get_data(type="same"):
         values_md = np.array([0.4909, 0.4184])
         std_devs_md = np.array([0.1331, 0.1607])
         labels = [
-            r"$\textbf{\textsc{BINARY}(2, 2) \rightarrow \textsc{GENDER}(1, 1)}$",
-            r"$\textbf{\textsc{CORR}(2, 1) \rightarrow \textsc{LANG}(1, 1)}$",
+            r"\textbf{\textsc{BINARY}(2, 2) $\rightarrow$ \textsc{GENDER}(1, 1)}",
+            r"\textbf{\textsc{CORR}(2, 1) $\rightarrow$ \textsc{LANG}(1, 1)}",
         ]
+        dataset_positions = np.array([3, 4.5])
 
     else:
         raise ValueError("Invalid type")
@@ -68,7 +70,6 @@ def get_data(type="same"):
         == len(values_aff)
         == len(values_eta)
     )
-    dataset_positions = np.array([2 * i + 1 for i in range(len(labels))])
     return (
         values_eta,
         std_devs_eta,
@@ -93,93 +94,118 @@ def main(args):
         dataset_positions,
     ) = get_data(type=args.datatype)
 
-    sns.set_theme(style="whitegrid")
+    sns.set_style("darkgrid")
     _, ax = plt.subplots(figsize=(15, 7))
-
-    width = 0.2
+    ax.xaxis.grid(False)
+    width = 0.35
+    ax.bar(
+        dataset_positions - width,
+        values_eta,
+        label=r"$\mathbf{\tilde{z}_{\eta}}$",
+        color="mediumslateblue",
+        edgecolor="gray",
+        linewidth=1.5,
+        width=0.4,
+    )
     ax.errorbar(
         dataset_positions - width,
         values_eta,
         yerr=std_devs_eta,
         fmt="o",
-        markersize=9,
-        label=r"$\mathbf{\tilde{z}_{\eta}}$",
-        color="mediumslateblue",
+        linestyle="",
+        color="black",
+        elinewidth=2,
         capsize=9,
-        capthick=1.5,
-        linewidth=3.5,
+        capthick=2,
+    )
+
+    ax.bar(
+        dataset_positions,
+        values_aff,
+        label=r"$\mathbf{\tilde{z}_{aff}}$",
+        color="lightseagreen",
+        edgecolor="gray",
+        linewidth=1.5,
+        width=0.4,
     )
     ax.errorbar(
         dataset_positions,
         values_aff,
         yerr=std_devs_aff,
         fmt="o",
-        markersize=9,
-        label=r"$\mathbf{\tilde{z}_{aff}}$",
-        color="lightseagreen",
+        linestyle="",
+        color="black",
+        elinewidth=2,
         capsize=9,
-        capthick=1.5,
-        linewidth=3.5,
+        capthick=2,
+    )
+    ax.bar(
+        dataset_positions + width,
+        values_md,
+        yerr=std_devs_md,
+        label=r"$\mathbf{\tilde{z}_{MD}}$",
+        color="hotpink",
+        capsize=9,
+        edgecolor="gray",
+        linewidth=1.5,
+        width=0.4,
     )
     ax.errorbar(
         dataset_positions + width,
         values_md,
         yerr=std_devs_md,
         fmt="o",
-        markersize=9,
-        label=r"$\mathbf{\tilde{z}_{MD}}$",
-        color="hotpink",
+        linestyle="",
+        color="black",
+        elinewidth=2,
         capsize=9,
-        capthick=1.5,
-        linewidth=3.5,
+        capthick=2,
     )
     ax.set_xticks(dataset_positions)
 
     ax.set_xticklabels(
         labels=labels,
     )
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-    ax.set_xlim([0, 12])
 
     ax.set_ylim([0, 0.9])
-    ax.set_ylabel(
+    ax.set_title(
         r"\textbf{Cosine Similarity} $\mathbf{\boldsymbol{\theta}(\tilde{z}, \circ)}$ (Higher is better)",
         fontsize=25,
-        labelpad=20,
+        pad=15,
     )
     ax.set_yticklabels(
         labels=[
-            r"\textbf{0.0}",
-            r"\textbf{0.1}",
-            r"\textbf{0.2}",
-            r"\textbf{0.3}",
-            r"\textbf{0.4}",
-            r"\textbf{0.5}",
-            r"\textbf{0.6}",
-            r"\textbf{0.7}",
-            r"\textbf{0.8}",
-            r"\textbf{0.9}",
+            r"$\textbf{0.0}$",
+            r"$\textbf{0.1}$",
+            r"$\textbf{0.2}$",
+            r"$\textbf{0.3}$",
+            r"$\textbf{0.4}$",
+            r"$\textbf{0.5}$",
+            r"$\textbf{0.6}$",
+            r"$\textbf{0.7}$",
+            r"$\textbf{0.8}$",
+            r"$\textbf{0.9}$",
         ],
     )
 
     ax.legend(
         title=r"$\textbf{Steering Method} (\circ)$",
-        title_fontsize=19,
+        title_fontsize=16,
         loc=2,
         prop={
-            "size": 23,
+            "size": 21,
             "weight": "bold",
         },
     )
-    ax.grid(
-        True,
-        which="major",
-        linestyle="-",
-        linewidth=1.0,
-        color="lightsteelblue",
-    )
-    for i in range(2, 2 * len(dataset_positions) - 1, 2):
-        ax.axvline(x=i, color="gray", linestyle="-", linewidth=1.5)
+    if args.datatype == "same":
+        for i in range(2, 2 * len(dataset_positions) - 1, 2):
+            ax.axvline(
+                x=i, color="lightslategray", linestyle="--", linewidth=0.9
+            )
+    elif args.datatype == "ood":
+        ax.axvline(
+            x=3.75, color="lightslategray", linestyle="--", linewidth=0.9
+        )
     ax.tick_params(axis="both", which="major", labelsize=19)
 
     plt.tight_layout()
