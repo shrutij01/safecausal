0894c55e55b37bbfcf56fa78227b0d67e5078ab5
Shruti Joshi <shrutijoshi98@gmail.com>
2025-08-05 12:37:20 -0400

    wip:steering

--- ssae/steer_response.py
+++ ssae/steer_response.py
@@ -3,14 +3,14 @@ from nnsight import LanguageModel
 llm = LanguageModel("meta-llama/Meta-Llama-3.1-8B", device_map="auto")
 print(llm)
 
-# with llm.trace("The Eiffel Tower is in the city of", remote=True):
-#     llm.model.layers[7].output[0][:] = 4
-#     output = llm.output.save()
+with llm.trace("The Eiffel Tower is in the city of", remote=True):
+    llm.model.layers[7].output[0][:] = 4
+    output = llm.output.save()
 
-# output_logits = output["logits"]
-# print("Model Output Logits: ", output_logits[0])
+output_logits = output["logits"]
+print("Model Output Logits: ", output_logits[0])
 
-# # decode the final model output from output logits
-# max_probs, tokens = output_logits[0].max(dim=-1)
-# word = [llm.tokenizer.decode(tokens.cpu()[-1])]
-# print("Model Output: ", word[0])
+# decode the final model output from output logits
+max_probs, tokens = output_logits[0].max(dim=-1)
+word = [llm.tokenizer.decode(tokens.cpu()[-1])]
+print("Model Output: ", word[0])
