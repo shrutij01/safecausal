927e25ca9292066720a3a16b8a7cf3d5f491ea47
Shruti Joshi <shrutijoshi98@gmail.com>
2025-08-05 12:42:47 -0400

    wip:steering

--- ssae/steer_response.py
+++ ssae/steer_response.py
@@ -1,16 +1,24 @@
 from nnsight import LanguageModel
+import debug_tools as dbg
 
-llm = LanguageModel("meta-llama/Meta-Llama-3.1-8B", device_map="auto")
-print(llm)
 
-with llm.trace("The Eiffel Tower is in the city of", remote=True):
-    llm.model.layers[7].output[0][:] = 4
-    output = llm.output.save()
+def main():
+    llm = LanguageModel("meta-llama/Meta-Llama-3.1-8B", device_map="auto")
+    print(llm)
 
-output_logits = output["logits"]
-print("Model Output Logits: ", output_logits[0])
+    with llm.trace("The Eiffel Tower is in the city of", remote=True):
+        llm.model.layers[7].output[0][:] = 4
+        output = llm.output.save()
 
-# decode the final model output from output logits
-max_probs, tokens = output_logits[0].max(dim=-1)
-word = [llm.tokenizer.decode(tokens.cpu()[-1])]
-print("Model Output: ", word[0])
+    output_logits = output["logits"]
+    print("Model Output Logits: ", output_logits[0])
+
+    # decode the final model output from output logits
+    max_probs, tokens = output_logits[0].max(dim=-1)
+    word = [llm.tokenizer.decode(tokens.cpu()[-1])]
+    print("Model Output: ", word[0])
+
+
+if __name__ == "__main__":
+    with dbg.debug_on_exception():
+        main()
