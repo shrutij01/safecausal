87b4559512fe999a828f8806063bed0f17744e02
Shruti Joshi <shrutijoshi98@gmail.com>
2025-08-05 12:49:23 -0400

    wip:steering

--- ssae/steer_response.py
+++ ssae/steer_response.py
@@ -1,22 +1,32 @@
 from nnsight import LanguageModel
 import debug_tools as dbg
+from transformers import AutoTokenizer
 
 
 def main():
     llm = LanguageModel("meta-llama/Meta-Llama-3.1-8B", device_map="auto")
     print(llm)
 
-    with llm.trace("The Eiffel Tower is in the city of", remote=True):
-        llm.model.layers[7].output[0][:] = 4
-        output = llm.output.save()
+    # with llm.trace("The Eiffel Tower is in the city of", remote=True):
+    #     llm.model.layers[7].output[0][:] = 4
+    #     output = llm.output.save()
 
-    output_logits = output["logits"]
-    print("Model Output Logits: ", output_logits[0])
+    # output_logits = output["logits"]
+    # print("Model Output Logits: ", output_logits[0])
 
-    # decode the final model output from output logits
-    max_probs, tokens = output_logits[0].max(dim=-1)
-    word = [llm.tokenizer.decode(tokens.cpu()[-1])]
-    print("Model Output: ", word[0])
+    # # decode the final model output from output logits
+    # max_probs, tokens = output_logits[0].max(dim=-1)
+    # word = [llm.tokenizer.decode(tokens.cpu()[-1])]
+    # print("Model Output: ", word[0])
+
+    model = LanguageModel("meta-llama/Llama-2-7b-hf", device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+    inputs = tokenizer("The universe is", return_tensors="pt")
+
+    with model.trace() as tracer:
+        output = model(**inputs)
+    print("Model Output: ", output)
 
 
 if __name__ == "__main__":
